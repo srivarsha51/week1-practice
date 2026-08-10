a47dde8e932585158ec4ce82f5bc296c1eac8bc8
Your Name <your_email@gmail.com>
2026-08-10 19:52:46 +0530

    Completed question 3

---
+++ question3.py
@@ -0,0 +1,18 @@
+n = int(input("Enter number: "))
+
+even_count = 0
+odd_count = 0
+
+for i in range(1, 11):
+
+    result = n * i
+
+    if result % 2 == 0:
+        print(n, "x", i, "=", result, "- Even")
+        even_count += 1
+    else:
+        print(n, "x", i, "=", result, "- Odd")
+        odd_count += 1
+
+print("\nEven Results:", even_count)
+print("Odd Results:", odd_count)
